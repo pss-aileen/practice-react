84c917137c769cd74d70c9c84405771c70cf4f52
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2024-02-07 18:08:08 +0800

    📖 ADD: 00_folder 更新
    script.jsを分割

---
+++ 00_folder/script.js
@@ -0,0 +1,13 @@
+"use strict";
+{
+  const App = () => {
+    return (
+      <h1>hello world</h1>
+    );
+  };
+  const container = document.getElementById("root");
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <App />
+  );
+}
