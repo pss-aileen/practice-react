9b41e15a967f93ffa1ba829dc8559707605cb182
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2024-02-12 09:54:06 +0800

    💕 FEAT: 基本的なAPIを取得する方法を実装 (#16)
    Udemyを見ながらなので、非同期処理の動きを再度確認する必要があると思った

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
+import { useEffect, useState } from "react";
+import { getAllPokemon, getSinglePokemon } from "./utils/pokemon";
 
 function App() {
+  const [pokemonData, setpokemonData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const initialURL = "https://pokeapi.co/api/v2/pokemon/";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      loadPokemon(res.results);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, [])
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getSinglePokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setpokemonData(_pokemonData);
+  }
+
+  console.log(pokemonData);
+
   return (
     <div className="App">
-      App
+      {loading ?
+        <>
+          <h1>loading...</h1>
+        </>
+        :
+        <>
+          <h1>fetched pokemon data</h1>
+          {pokemonData.map((data) => {
+            return (
+              <>
+                <img src={data.sprites.front_default} width="160"/>
+                <img src={data.sprites.back_default} width="160"/>
+                <br />
+              </>
+            )
+          })}
+        </>
+      }
     </div>
   );
 }
---
+++ src/utils/pokemon.js
@@ -0,0 +1,20 @@
+export const getAllPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data));
+  });
+}
+
+
+export const getSinglePokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => { 
+        resolve(data)
+        console.log(data);
+      }
+      );
+  });
+}
