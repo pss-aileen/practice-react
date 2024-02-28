cb23b0057c5f04717e221e1ef67d8706f226bc52
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2024-02-28 17:04:05 +0800

    💕 FEAT: ParentComponentとChildComponetセット (#23)

---
+++ src/Components/ChildComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChildComponent = () => {
+  return (
+    <div>ChildComponent</div>
+  )
+}
+
+export default ChildComponent
---
+++ src/Components/ParentComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ParentComponent = () => {
+  return (
+    <div>ParentComponent</div>
+  )
+}
+
+export default ParentComponent
